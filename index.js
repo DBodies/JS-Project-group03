7e24849ec3a221e90e740a073360e6781286634f
DBodies <159953887+DBodies@users.noreply.github.com>
2025-04-06 17:48:28 +0000

    Deploying to gh-pages from @ DBodies/JS-Project-group03@d175b6895c044f8f3ff54391faebb5fa2081d4b8 🚀

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-import"./assets/vendor-CjZ6FJNh.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{S as a,a as u,i as p}from"./assets/vendor-DhkyhFKW.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&n(l)}).observe(document,{childList:!0,subtree:!0});function c(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=c(e);fetch(e.href,r)}})();const o=document.querySelector(".slider-nav-prev"),d=document.querySelector(".slider-nav-next");o.disabled=!0;d.addEventListener("click",()=>{s.slideNext()});o.addEventListener("click",()=>{s.slidePrev()});document.addEventListener("keydown",function(t){t.key==="ArrowLeft"&&s.slidePrev(),t.key==="ArrowRight"&&s.slideNext()});w();const s=new a(".swiper-container",{grabCursor:!0,speed:500,slidesPerView:1,spaceBetween:16,breakpoints:{375:{slidesPerView:1,spaceBetween:16},768:{slidesPerView:2,spaceBetween:16},1440:{slidesPerView:4,spaceBetween:16}},on:{slideChange:function(){o.disabled=!1,d.disabled=!1,s.isEnd&&(d.disabled=!0),s.isBeginning&&(o.disabled=!0)}}});function f(t){const i=document.querySelector(".swiper-wrapper"),c=t.map(({author:n,avatar_url:e,review:r})=>`
+           <li class="swiper-slide slider-item">
+                <img class="slider-item-img" src="${e}">
+                <p class="slider-item-title">${n}</p>
+                <p class="slider-item-info">${r}</p>
+            </li>             
+            `).join("");i.insertAdjacentHTML("beforeend",c)}function m(){document.querySelector(".swiper-wrapper").insertAdjacentHTML("beforeend",`
+  
+  <div class="slider-error">
+    Not found!
+  </div>  
+  
+  `)}async function w(){try{debugger;const t=await u.get("https://portfolio-js.b.goit.study/api/reviews");f(t.data)}catch{m(),o.disabled=!0,d.disabled=!0,p.error({title:"Error",message:"Something wrong, try again later!"})}}
 //# sourceMappingURL=index.js.map
